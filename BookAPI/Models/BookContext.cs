fad83e1acbdfa506d61fa0aca2f701377fecff9b
Mauricio <mauricio8_1996@hotmail.com>
2021-12-28 15:05:19 -0400

    Agregar archivos de proyecto.

---
+++ BookAPI/Models/BookContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookAPI.Models
+{
+    public class BookContext :DbContext
+    {
+        public BookContext(DbContextOptions<BookContext> options)
+            :base(options)
+        {
+            Database.EnsureCreated();
+        }
+        public DbSet<Book> Books { get; set; }
+        
+    }
+}
